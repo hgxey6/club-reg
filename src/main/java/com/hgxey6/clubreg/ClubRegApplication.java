5c742155b8c16edd68e84677dd49954e56e47976
Bibarys <hgxey6@gmail.com>
2021-10-25 21:57:12 +0600

    add the spring skeleton to the project

---
+++ src/main/java/com/hgxey6/clubreg/ClubRegApplication.java
@@ -0,0 +1,13 @@
+package com.hgxey6.clubreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgxey6/clubreg/ClubRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgxey6.clubreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
